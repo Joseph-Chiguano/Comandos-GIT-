89270c013ab159349bbebe5cad68ab434b4e841e
Joseph-Chiguano <141870297+Joseph-Chiguano@users.noreply.github.com>
2023-11-28 14:29:24 -0500

    Add files via upload

---
+++ Carro/src/carro/NewMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package carro;
+
+/**
+ *
+ * @author usuario
+ */
+public class NewMain {
+ public static void main(String[] args) {
+        Carro c1 =new Carro ("BMW" , 907);//instancia=dar vida
+        c1.acelerar ();
+       }
+ }
